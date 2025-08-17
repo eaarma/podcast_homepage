f218a1d03b92bdabfa7e5100fcb1e60f6f3de116
Ed <edward5445@gmail.com>
2025-08-17 21:11:09 +0300

    final fix to admin login navigation url

--- backend/testHash.js
+++
@@ -1,6 +0,0 @@
-import bcrypt from "bcryptjs";
-
-const hash = "$2b$12$9w.xDxji.Si89BDiVzsVN.lbiPl219NKB4kltv5Hes2m7Eswu2HIW";
-const password = "parool123";
-
-console.log(bcrypt.compareSync(password, hash)); // should print true
