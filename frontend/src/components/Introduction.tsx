275501127700f06aa93d694781c861b52c927f95
Ed <edward5445@gmail.com>
2025-08-24 21:04:17 +0300

    fixed frontend title size issue

--- frontend/src/components/Introduction.tsx
+++ frontend/src/components/Introduction.tsx
@@ -6,9 +6,7 @@ interface IntroductionProps {
 const Introduction: React.FC<IntroductionProps> = ({ title, description }) => {
   return (
     <div className="max-w-3xl mx-auto px-4 text-center my-8">
-      <h1 className="text-2xl font-bold mb-4 whitespace-nowrap overflow-hidden text-ellipsis">
-        {title}
-      </h1>
+      <h1 className="text-xl md:text-2xl font-bold mb-4 truncate">{title}</h1>{" "}
       <p className="text-lg">{description}</p>
     </div>
   );
